29d0721dc13ec96d2a5ce666e68505d07a17bf0b
meiyarasu1997 <meiyarasuarun@gmail.com>
2020-11-27 18:22:57 +0530

    commit

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import flask
 from flask import Flask, render_template, url_for, request, redirect
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sendemail import send_mail
 app = Flask(__name__)
 ENV = 'dev'
-
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost/tesDemo'
@@ -13,6 +13,7 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jlufufnyratasp:e3ee5445da3626f94e0df13e14696139e070591b1c51921220b8323461ebbbca@ec2-34-200-106-49.compute-1.amazonaws.com:5432/dcuebo1er73g9b'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+applicationemail='aravinth.balajee@gmail.com'
 
 db = SQLAlchemy(app)
 
@@ -64,6 +65,8 @@ def newuser():
             data = User_details(first_Name,last_Name,cws_id ,email_id,access_level,password)
             db.session.add(data)
             db.session.commit()
+            send_mail(email_id, 'Sign Up Successfull:Swift System',
+                      'You have successfully signed up to Mobile Authentication System')
             msg = "Sign up successfull"
             return render_template('index.html',msg=msg)
         else:
@@ -80,22 +83,23 @@ def newuser():
 def login():
     if request.method == 'POST':
       try:
-        user_name= request.form['email']
+        cws_id = request.form['email']
+        email_id = request.form['email']
         password = request.form['psw']
-        print(user_name,password)
-        selected_user = db.session.query(User_details).filter(User_details.CWS_id == user_name).first()
+        #print(user_name,password)
+        selected_user = db.session.query(User_details).filter(User_details.CWS_id == cws_id).first()
         if(not selected_user):
-            selected_user = db.session.query(User_details).filter(User_details.Email_id == user_name).first()
+            selected_user = db.session.query(User_details).filter(User_details.Email_id == email_id).first()
             if(not selected_user):
                 msg = "Incorrect username/password!"
-                return render_template('index.html', msg=msg)
+                return render_template('index.html', msg=msg,email=email_id)
 
         if (selected_user.Password==password):
             msg = "sucessfully logged in"
             return render_template('index.html', msg=msg)
         else:
             msg = "Incorrect username/password!"
-            return render_template('index.html', msg=msg)
+            return render_template('index.html', msg=msg,email=email_id)
       except:
             return 'Error occuered while logging in'
     else:
